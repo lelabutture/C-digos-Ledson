8dce56dfca16e968c90ac4b2377b2557005483f8
lela <130366723+lelabutture@users.noreply.github.com>
2024-09-29 15:12:19 -0300

    adicionado desafio cjs

---
+++ desafioCJS/index.js
@@ -0,0 +1,9 @@
+import{mostrarDadosVeiculo, acelerar} from './veiculo.js';
+import {fs} from './modulo.cjs';
+const DadosVeiculo =
+mostrarDadosVeiculo("renault", "clio", "renault_bra")
+const aceleração = acelerar(100)
+var ads = fs
+console.log (DadosVeiculo)
+console.log (aceleração)
+console.log (ads)
---
+++ desafioCJS/veiculo.js
@@ -0,0 +1,7 @@
+export function acelerar(velocidade) {
+  return `${velocidade} km/h`;
+}
+
+export function mostrarDadosVeiculo(marca, modelo, fabricante) {
+  return `marca: ${marca},modelo: ${modelo}, fabricante: ${fabricante}`;
+}
